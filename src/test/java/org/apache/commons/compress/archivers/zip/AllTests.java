7680a083e4d61b238e393b61a2a67ec14fabc663
Daisy Zhang1 <2790883233@qq.com>
2025-04-19 12:02:31 +0800

    aaa

--- src/test/java/org/apache/commons/compress/archivers/zip/AllTests.java
+++ src/test/java/org/apache/commons/compress/archivers/zip/AllTests.java
@@ -1,10 +1,12 @@
 package org.apache.commons.compress.archivers.zip;
 
+import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 import org.junit.runner.RunWith;
 
+@RunWith(JUnitPlatform.class)  // 添加运行器
 @Suite
 @SelectClasses({TestZipArchiveEntryJunit.class})
 @IncludeTags("test_Entry")
